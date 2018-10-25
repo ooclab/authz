0a2eee1e042fe16293e4dc53fe54490522edeac4
gwind <lijian.gnu@gmail.com>
2018-10-25 15:13:27 +0800

    根据环境变量决定是否需要 syncdb

--- src/codebase/settings.py
+++ src/codebase/settings.py
@@ -2,6 +2,7 @@ DEBUG = True
 SECRET_KEY = "Unmy6tuxgIAPX7zxlp80MxcDz1mofni2chnrF4cxMQQjq"
 SERVER_EMAIL = "info@ooclab.com"
 
+SYNC_DATABASE = True
 # http://docs.sqlalchemy.org/en/latest/core/engines.html
 DB_URI = "sqlite://"
 # DB_URI = "postgresql://root:password@postgres:5432/authz"
--- src/server.py
+++ src/server.py
@@ -1,13 +1,21 @@
 #! /usr/bin/env python3
 
 import logging
+from importlib import import_module
 
 import tornado.options
+from eva.conf import settings
 
 from codebase.app import make_app
+from codebase.utils.sqlalchemy import dbc
 
 
 def main():
+
+    # sync database
+    if settings.SYNC_DATABASE:
+        import_module(settings.MODELS_MODULE)
+        dbc.create_all()
 
     # 启动 Tornado
     app = make_app()
