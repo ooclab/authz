c2d3364b61aec4a9784cbe15f019daa3aa5af7fa
gwind <lijian.gnu@gmail.com>
2018-10-25 16:16:55 +0800

    stop tornado we server gracefully

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
+import time
+import signal
 import logging
+from functools import partial
 from importlib import import_module
 
 import tornado.options
@@ -8,6 +11,35 @@ from eva.conf import settings
 
 from codebase.app import make_app
 from codebase.utils.sqlalchemy import dbc
+
+MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 0
+
+
+# how to shutdown tornado web server gracefully, ref:
+# - https://gist.github.com/wonderbeyond/d38cd85243befe863cdde54b84505784
+# - https://gist.github.com/mywaiting/4643396
+def sig_handler(server, sig, _):
+    io_loop = tornado.ioloop.IOLoop.instance()
+
+    def stop_loop(deadline):
+        now = time.time()
+        # TODO: 怎样判断可以立即退出？
+        if now < deadline:
+            logging.info('Waiting for next tick')
+            io_loop.add_timeout(now + 1, stop_loop, deadline)
+        else:
+            io_loop.stop()
+            logging.info('Shutdown finally')
+
+    def shutdown():
+        logging.info('Stopping http server')
+        server.stop()
+        logging.info('Will shutdown in %s seconds ...',
+                     MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+        stop_loop(time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+
+    logging.warning('Caught signal: %s', sig)
+    io_loop.add_callback_from_signal(shutdown)
 
 
 def main():
@@ -29,6 +61,10 @@ def main():
 
     port = tornado.options.options.port
     sockets = tornado.netutil.bind_sockets(port)
+
+    # 监听信号，优雅退出
+    signal.signal(signal.SIGTERM, partial(sig_handler, server))
+    signal.signal(signal.SIGINT, partial(sig_handler, server))
 
     server.add_sockets(sockets)
     logging.info("api server is running at %d", port)
