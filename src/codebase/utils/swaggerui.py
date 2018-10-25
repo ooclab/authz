133892e176731efea87c1c4fc0ca38813d7051ff
gwind <lijian.gnu@gmail.com>
2018-10-25 11:41:25 +0800

    先前由服务自己检查 request 参数需要，以后使用 ga 统一检查请求参数，不再需要该方法

--- src/codebase/utils/swaggerui.py
+++ src/codebase/utils/swaggerui.py
@@ -1,4 +1,4 @@
-# pylint: disable=C0103,R0201
+# pylint: disable=C0103,R0201,R0903
 
 import os
 
@@ -9,6 +9,7 @@ from bravado_core.validate import validate_object
 
 
 class Api:
+
     def __init__(self):
 
         # TODO: eva.conf.settings.ROOT_PATH 有 bug, 显示的是 python3 的路径
@@ -19,13 +20,6 @@ class Api:
 
     def validate_object(self, object_spec, value):
         validate_object(self.spec, object_spec, value)
-
-    def get_request_schema(self, op_spec, name):
-        for param in op_spec["parameters"]:
-            if param["name"] == name:
-                return param["schema"]
-        # TODO: 统一错误信息
-        raise Exception(f"no-such-request:{name}")
 
 
 api = Api()
