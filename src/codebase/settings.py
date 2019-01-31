d3479dbcfd3bfa8d020b77fb1ac20ca2e122b2cd
gwind <gwind@ooclab.com>
2019-01-31 12:56:17 +0800

    在 settings 中统一使用字符串替代 bool 值

--- src/codebase/settings.py
+++ src/codebase/settings.py
@@ -1,8 +1,9 @@
-DEBUG = True
+DEBUG = "false"
+CORS = "false"
 SECRET_KEY = "Unmy6tuxgIAPX7zxlp80MxcDz1mofni2chnrF4cxMQQjq"
 SERVER_EMAIL = "info@ooclab.com"
 
-SYNC_DATABASE = True
+SYNC_DATABASE = "false"
 # http://docs.sqlalchemy.org/en/latest/core/engines.html
 DB_URI = "sqlite://"
 # DB_URI = "postgresql://root:password@postgres:5432/authz"
